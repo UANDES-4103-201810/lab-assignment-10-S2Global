cf5f01f9727e937730308196c7a05f8e30c6d758
Francisco Alvarez <s2globalmail@gmail.com>
2018-05-29 14:58:28 -0400

    All but user show.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    where ( provider auth.provider,uid:auth.uid).first or_create do | user |
+    where(provider auth.provider,uid:auth.uid).first or_create do | user |
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
